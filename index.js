2401698f86cd9584d67f22fe76e5800769a87aaa
jonschlinkert <jon.schlinkert@sellside.com>
2016-09-01 16:53:22 -0400

    expose `nocolor`

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 'use strict';
 
-var isWindows = process.platform === 'win32';
+var green = require('ansi-green');
+var gray = require('ansi-gray');
+var win = process.platform === 'win32';
+
 module.exports = {
-  on: isWindows ? '(*)' : '◉',
-  off: isWindows ? '( )' : '◯',
+  on: green(win ? '(*)' : '◉'),
+  off: win ? '( )' : '◯',
+  disabled: gray(win ? '(x)' : 'ⓧ')
+};
+
+module.exports.star = {
+  on: green('★'),
+  off: '☆',
+  disabled: gray('☆')
+};
+
+module.exports.ballot = {
+  on: green('☑'),
+  off: '☐',
+  disabled: gray('☒')
+};
+
+module.exports.nocolor = {
+  on: win ? '(*)' : '◉',
+  off: win ? '( )' : '◯',
+  disabled: win ? '(x)' : 'ⓧ',
+
+  star: {
+    on: green('★'),
+    off: '☆',
+    disabled: gray('☆')
+  },
+
+  ballot: {
+    on: green('☑'),
+    off: '☐',
+    disabled: gray('☒')
+  }
 };
