42a7056cbfdc77d1db5ecf6323112c8f184f7fa2
jonschlinkert <jon.schlinkert@sellside.com>
2017-05-21 09:19:42 -0400

    adds `check` radio group

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var gray = require('ansi-gray');
 module.exports = {
   on: green(win ? '(*)' : '◉'),
   off: win ? '( )' : '◯',
-  disabled: gray(win ? '(x)' : 'ⓧ')
+  disabled: gray(win ? '(|)' : 'Ⓘ')
 };
 
 module.exports.star = {
@@ -16,19 +16,25 @@ module.exports.star = {
   disabled: gray('☆')
 };
 
+module.exports.check = {
+  on: green(win ? '\u221A' : '✓'),
+  off: win ? '[ ]' : '☐',
+  disabled: gray(win ? '\u00D7' : '☒')
+};
+
 module.exports.ballot = {
-  on: green('☑'),
-  off: '☐',
-  disabled: gray('☒')
+  on: green(win ? '[×]' : '☑'),
+  off: win ? '[ ]' : '☐',
+  disabled: gray(win ? '\u00D7' : '☒')
 };
 
 module.exports.nocolor = {
   on: win ? '(*)' : '◉',
   off: win ? '( )' : '◯',
-  disabled: win ? '(x)' : 'ⓧ',
+  disabled: win ? '(|)' : 'Ⓘ',
 
   star: {
-    on: '★',
+    on: win ? '*' : '★',
     off: '☆',
     disabled: '☆'
   },
@@ -37,5 +43,11 @@ module.exports.nocolor = {
     on: '☑',
     off: '☐',
     disabled: '☒'
+  },
+
+  check: {
+    on: win ? '\u221A' : '✓',
+    off: win ? '[ ]' : '☐',
+    disabled: win ? '\u00D7' : '☒'
   }
 };
--- test.js
+++ test.js
@@ -17,11 +17,24 @@ describe('radio-symbol', function() {
     if (isWindows()) {
       assert.equal(radio.on, green('(*)'));
       assert.equal(radio.off, '( )');
-      assert.equal(radio.disabled, gray('(x)'));
+      assert.equal(radio.disabled, gray('(|)'));
     } else {
       assert.equal(radio.on, green('◉'));
       assert.equal(radio.off, '◯');
-      assert.equal(radio.disabled, gray('ⓧ'));
+      assert.equal(radio.disabled, gray('Ⓘ'));
+    }
+  });
+
+  it('should export check symbols', function() {
+    if (isWindows()) {
+      assert.equal(radio.check.on, green('\u221A'));
+      assert.equal(radio.check.off, '[ ]');
+      assert.equal(radio.check.disabled, gray('\u00D7'));
+    } else {
+      assert.equal(radio.check.on, green('✓'));
+      assert.equal(radio.check.off, '☐');
+      assert.equal(radio.check.disabled, gray('☒'));
     }
   });
 });
+
